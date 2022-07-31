3284693d4f18fd42e5daa5d2335f9daaed60b35f
trishna0703 <trishna0703@gmail.com>
2022-07-31 20:02:51 +0530

    Final

--- src/skyscraperConstruction/driver/Driver.java
+++ src/skyscraperConstruction/driver/Driver.java
@@ -8,20 +8,35 @@ public class Driver {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        List<Integer> floors = new LinkedList();
+        List<Integer> floors = new LinkedList<>();
         List<Integer> sortedFloors = new LinkedList<>();
 
-        System.out.println("Enter the no. of floors in the building.");
+        //Taking input for floor count
+        int floorCount = 0;
+        boolean invalidInput = false;
+        do {
+            if(invalidInput) {
+                System.out.println("That's not a valid positive number, please enter the total no. of floors in the building.");
+            }else {
+                System.out.println("Enter the total no. of floors in the building.");
+            }
 
-        int noOfFloors = 0;
-        if(in.hasNextInt()){
-            noOfFloors = in.nextInt();
-        }else{
-            System.out.println("Enter a numeric value");
-            main(args);
-        }
-        for (int i = 0; i < noOfFloors;i++) {
-            System.out.println("Enter the floor size on day: " + (i + 1));
+            while (!in.hasNextInt()) {
+                System.out.println("That's not a number, please enter the total no. of floors in the building.");
+                in.next();
+            }
+
+            floorCount = in.nextInt();
+            if(floorCount <= 0) {
+                invalidInput = true;
+            } else {
+                invalidInput = false;
+            }
+        } while (invalidInput);
+
+        //Taking floor sizes
+        for (int i = 0; i < floorCount;i++) {
+            System.out.println("Enter the floor size on day " + (i + 1) + ":");
             int nextFloor = -1;
             try{
                 nextFloor =  in.nextInt();
@@ -30,6 +45,7 @@ public class Driver {
                 i--;
                 in.nextLine();
             }
+            //validating unique floor sizes
             if(floors.contains(nextFloor)){
                 System.out.println("Size " +nextFloor + " already exists in the building. Please enter a different size.");
                 i--;
@@ -45,6 +61,4 @@ public class Driver {
         OrderOfConstruction.FindOrderOfConstruction(floors, sortedFloors);
         in.close();
     }
-
-
 }
